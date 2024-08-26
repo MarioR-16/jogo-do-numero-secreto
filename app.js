821d0a516317e54fd632472146d978d588f3264b
MarioR-16 <mario.rian0305@gmail.com>
2024-08-26 16:38:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ function verificarChute() {
         document.getElementById("reiniciar").removeAttribute("disabled");
     } else {
         if (chute > numeroSecreto) {
-            exibirTextoNaTela("p", "O número secreto é maior");
-            exibirTextoNaTela("P", "O número secreto é menor");
+            exibirTextoNaTela("p", "O número secreto é menor");
+            exibirTextoNaTela("P", "O número secreto é maior");
         }
         tentativas++;
         limparCampo();
